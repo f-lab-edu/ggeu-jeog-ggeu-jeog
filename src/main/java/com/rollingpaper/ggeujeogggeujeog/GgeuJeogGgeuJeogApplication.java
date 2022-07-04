3ab3a95394b58dba22dd939296c316c603b88517
namuhuchutong <namuhuchutong@gmail.com>
2022-07-05 02:31:42 +0900

    #1 Chore: 기본 환경 설정

---
+++ src/main/java/com/rollingpaper/ggeujeogggeujeog/GgeuJeogGgeuJeogApplication.java
@@ -0,0 +1,13 @@
+package com.rollingpaper.ggeujeogggeujeog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GgeuJeogGgeuJeogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GgeuJeogGgeuJeogApplication.class, args);
+	}
+
+}
