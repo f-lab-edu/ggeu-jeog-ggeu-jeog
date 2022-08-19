e388868cb1e45930f55918aba392e3dbc1690759
namuhuchutong <namuhuchutong@gmail.com>
2022-08-20 02:28:10 +0900

    Feat: 테스트 픽스처 클래스 추가

---
+++ src/test/java/com/rollingpaper/ggeujeogggeujeog/common/fixture/BoardTestFixture.java
@@ -0,0 +1,17 @@
+package com.rollingpaper.ggeujeogggeujeog.common.fixture;
+
+import com.rollingpaper.ggeujeogggeujeog.board.domain.Board;
+import com.rollingpaper.ggeujeogggeujeog.board.domain.Theme;
+
+public class BoardTestFixture {
+
+	public static class TestBoard {
+		public static Board BOARD1 = Board.builder()
+			.id(1L)
+			.boardTitle("test-title-1")
+			.theme(Theme.THEME1)
+			.isOpened(false)
+			.userId(UserTestFixture.TestUser.USER1.getId())
+			.build();
+	}
+}
---
+++ src/test/java/com/rollingpaper/ggeujeogggeujeog/common/fixture/ImageTestFixture.java
@@ -0,0 +1,16 @@
+package com.rollingpaper.ggeujeogggeujeog.common.fixture;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import com.rollingpaper.ggeujeogggeujeog.common.factory.ImageFile;
+
+public class ImageTestFixture {
+
+	public static String IMAGE_NAME1 = "test_image1.jpeg";
+	public static MultipartFile MOCK_JPEG_FILE
+		= ImageFile.MockImageFileFactory(IMAGE_NAME1, ImageFile.ImageFormat.JPEG);
+
+	public static String IMAGE_NAME2 = "test_image2.png";
+	public static MultipartFile MOCK_PNG_FILE
+		= ImageFile.MockImageFileFactory(IMAGE_NAME2, ImageFile.ImageFormat.PNG);
+}
