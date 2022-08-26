e7c7e1ebaf4a027012a6cf995ce50ab9362bae28
namuhuchutong <namuhuchutong@gmail.com>
2022-08-27 04:22:23 +0900

    Refactor: 페이퍼 텍스처 2 추가

--- src/test/java/com/rollingpaper/ggeujeogggeujeog/common/fixture/PaperTestFixture.java
+++ src/test/java/com/rollingpaper/ggeujeogggeujeog/common/fixture/PaperTestFixture.java
@@ -10,12 +10,24 @@ public class PaperTestFixture {
 		public static Paper PAPER1 = Paper.builder()
 			.id(1L)
 			.ownerName("test-owner-1")
-			.content("test-content")
-			.contentMeta("{meta:\"test\"")
+			.content("test-content-1")
+			.contentMeta("{\"meta\":\"test-1\"}")
 			.imageUrl(ImageTestFixture.IMAGE_NAME1)
 			.createdDate(LocalDateTime.now())
 			.updatedDate(LocalDateTime.now())
 			.userId(UserTestFixture.TestUser.USER1.getId())
+			.boardId(BoardTestFixture.TestBoard.BOARD1.getId())
+			.build();
+
+		public static Paper PAPER2 = Paper.builder()
+			.id(2L)
+			.ownerName("test-owner-2")
+			.content("test-content-2")
+			.contentMeta("{\"meta\":\"test-2\"}")
+			.imageUrl(ImageTestFixture.IMAGE_NAME2)
+			.createdDate(LocalDateTime.now())
+			.updatedDate(LocalDateTime.now())
+			.userId(UserTestFixture.TestUser.USER2.getId())
 			.boardId(BoardTestFixture.TestBoard.BOARD1.getId())
 			.build();
 	}
