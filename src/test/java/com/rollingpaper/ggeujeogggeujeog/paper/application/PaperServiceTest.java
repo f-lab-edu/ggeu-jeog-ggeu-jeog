daa11ebc330ccf208bff37445a9870249860d975
namuhuchutong <namuhuchutong@gmail.com>
2022-08-27 04:32:36 +0900

    Test: 페이퍼 수정, 삭제 테스트 케이스 추가

--- src/test/java/com/rollingpaper/ggeujeogggeujeog/paper/application/PaperServiceTest.java
+++ src/test/java/com/rollingpaper/ggeujeogggeujeog/paper/application/PaperServiceTest.java
@@ -19,6 +19,7 @@ import com.rollingpaper.ggeujeogggeujeog.common.fixture.ImageTestFixture;
 import com.rollingpaper.ggeujeogggeujeog.common.util.ImageStorage;
 import com.rollingpaper.ggeujeogggeujeog.paper.exception.NoSuchPaperException;
 import com.rollingpaper.ggeujeogggeujeog.paper.infrastructure.PaperMapper;
+import com.rollingpaper.ggeujeogggeujeog.paper.presentation.dto.PaperUpdateRequestDto;
 import com.rollingpaper.ggeujeogggeujeog.paper.presentation.dto.PaperWriteRequest;
 
 @ExtendWith(MockitoExtension.class)
@@ -116,5 +117,55 @@ class PaperServiceTest {
 
 		//then
 		then(paperMapper).should(times(1)).save(any());
+	}
+
+	@Test
+	@DisplayName("등록된 페이퍼를 id로 삭제한다.")
+	void deletePaperWithPaperId() {
+		//when
+		paperServiceImpl.delete(TestPaper.PAPER1.getId());
+
+		//then
+		then(paperMapper).should(times(1)).delete(anyLong());
+	}
+
+	@Test
+	@DisplayName("등록된 페이퍼를 새로운 이미지와 함께 수정한다.")
+	void updatePaperWithNewImage() {
+		//given
+		PaperUpdateRequestDto paperUpdateRequestDto = new PaperUpdateRequestDto(
+			TestPaper.PAPER2.getOwnerName(),
+			TestPaper.PAPER2.getContent(),
+			TestPaper.PAPER2.getContentMeta()
+		);
+		given(imageStorage.store(ImageTestFixture.MOCK_JPEG_FILE)).willReturn("/" + ImageTestFixture.IMAGE_NAME1);
+		given(paperMapper.findById(anyLong())).willReturn(Optional.of(TestPaper.PAPER1));
+
+		//when
+		paperServiceImpl.update(paperUpdateRequestDto, ImageTestFixture.MOCK_JPEG_FILE, TestPaper.PAPER1.getId());
+
+		//then
+		then(paperMapper).should(times(1)).update(any());
+		then(imageStorage).should(times(1)).store(any());
+	}
+
+	@Test
+	@DisplayName("등록된 페이퍼를 새로운 이미지 없이 수정한다.")
+	void updatePaperWithoutNewImage() {
+		//given
+		PaperUpdateRequestDto paperUpdateRequestDto = new PaperUpdateRequestDto(
+			TestPaper.PAPER2.getOwnerName(),
+			TestPaper.PAPER2.getContent(),
+			TestPaper.PAPER2.getContentMeta()
+		);
+		given(imageStorage.store(null)).willReturn("");
+		given(paperMapper.findById(anyLong())).willReturn(Optional.of(TestPaper.PAPER1));
+
+		//when
+		paperServiceImpl.update(paperUpdateRequestDto, null, TestPaper.PAPER1.getId());
+
+		//then
+		then(paperMapper).should(times(1)).update(any());
+		then(imageStorage).should(times(1)).store(any());
 	}
 }
