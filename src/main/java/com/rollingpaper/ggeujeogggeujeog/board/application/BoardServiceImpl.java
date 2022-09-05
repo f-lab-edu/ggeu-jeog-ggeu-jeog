2d342d46d91841ef091ca38afa7b062e79854705
namuhuchutong <namuhuchutong@gmail.com>
2022-09-05 18:42:57 +0900

    Refactor: Mapper 결과 타입을 dto -> 엔티티로 변경

--- src/main/java/com/rollingpaper/ggeujeogggeujeog/board/application/BoardServiceImpl.java
+++ src/main/java/com/rollingpaper/ggeujeogggeujeog/board/application/BoardServiceImpl.java
@@ -1,11 +1,12 @@
 package com.rollingpaper.ggeujeogggeujeog.board.application;
 
 import java.util.List;
-import java.util.Optional;
+import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.rollingpaper.ggeujeogggeujeog.board.domain.Board;
 import com.rollingpaper.ggeujeogggeujeog.board.infrastructure.BoardMapper;
 import com.rollingpaper.ggeujeogggeujeog.board.presentation.dto.BoardRequestDto;
 import com.rollingpaper.ggeujeogggeujeog.board.presentation.dto.UserBoardResponseDto;
@@ -24,19 +25,19 @@ public class BoardServiceImpl implements BoardService {
     private final BoardMapper boardMapper;
 
     @Override
-    public void register(BoardRequestDto dto, Long id) {
-        Optional<User> user = userService.getUserById(id);
-        boardMapper.save(BoardRequestDto.toEntity(dto, user.orElseThrow(NoSuchUserException::new).getId()));
+    public void register(BoardRequestDto dto, Long userId) {
+        User user = userService.getUserById(userId).orElseThrow(NoSuchUserException::new);
+        boardMapper.save(BoardRequestDto.toEntity(dto, user));
     }
 
     @Override
     @Transactional(readOnly = true)
-    public List<UserBoardResponseDto> getUserBoard(Long id) {
-        userService.getUserById(id)
-                .orElseThrow(NoSuchUserException::new);
-
-        return boardMapper.findByUserId(id);
-
+    public List<UserBoardResponseDto> getUserBoard(Long userId) {
+        List<Board> boards = boardMapper.findByUserId(userId);
+        return boards.stream()
+            .map(board -> new UserBoardResponseDto(board.getBoardTitle(),
+            board.getTheme(), board.isOpened(), board.getUpdatedDate()))
+            .collect(Collectors.toList());
     }
 
 }
--- src/main/java/com/rollingpaper/ggeujeogggeujeog/board/infrastructure/BoardMapper.java
+++ src/main/java/com/rollingpaper/ggeujeogggeujeog/board/infrastructure/BoardMapper.java
@@ -1,16 +1,15 @@
 package com.rollingpaper.ggeujeogggeujeog.board.infrastructure;
 
-import com.rollingpaper.ggeujeogggeujeog.board.domain.Board;
-import com.rollingpaper.ggeujeogggeujeog.board.presentation.dto.UserBoardResponseDto;
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
+import com.rollingpaper.ggeujeogggeujeog.board.domain.Board;
 
 @Repository
 @Mapper
 public interface BoardMapper {
     void save(Board board);
-
-    List<UserBoardResponseDto> findByUserId(Long userId);
+    List<Board> findByUserId(Long userId);
 }
