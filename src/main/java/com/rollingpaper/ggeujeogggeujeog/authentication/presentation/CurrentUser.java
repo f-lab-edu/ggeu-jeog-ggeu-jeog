3ee2371e48e22675deba3902a5bacd19eb3507cf
namuhuchutong <namuhuchutong@gmail.com>
2022-09-05 10:18:36 +0900

    Feat: 사용자 ArgumentResolver 구현

---
+++ src/main/java/com/rollingpaper/ggeujeogggeujeog/authentication/presentation/CurrentUser.java
@@ -0,0 +1,10 @@
+package com.rollingpaper.ggeujeogggeujeog.authentication.presentation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface CurrentUser {
+}
