08b15a44231f5e3b015aeab989797f5012201103
namuhuchutong <namuhuchutong@gmail.com>
2022-10-01 03:26:46 +0900

    Config: 메시지 전송 기능 설정 추가

--- src/main/java/com/rollingpaper/ggeujeogggeujeog/common/config/WebLocalConfig.java
+++ src/main/java/com/rollingpaper/ggeujeogggeujeog/common/config/WebLocalConfig.java
@@ -2,6 +2,7 @@ package com.rollingpaper.ggeujeogggeujeog.common.config;
 
 import java.util.List;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
@@ -9,6 +10,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.rollingpaper.ggeujeogggeujeog.authentication.application.LoginService;
 import com.rollingpaper.ggeujeogggeujeog.authentication.presentation.CurrentUserArgumentResolver;
+import com.rollingpaper.ggeujeogggeujeog.notification.domain.MessageSender;
+import com.rollingpaper.ggeujeogggeujeog.notification.infrastructure.FirebaseMessageSender;
 import com.rollingpaper.ggeujeogggeujeog.user.application.UserService;
 
 import lombok.RequiredArgsConstructor;
@@ -24,5 +27,10 @@ public class WebLocalConfig implements WebMvcConfigurer {
 	@Override
 	public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
 		resolvers.add(new CurrentUserArgumentResolver(loginService, userService));
+	}
+
+	@Bean
+	public MessageSender messageSender() {
+		return new FirebaseMessageSender();
 	}
 }
