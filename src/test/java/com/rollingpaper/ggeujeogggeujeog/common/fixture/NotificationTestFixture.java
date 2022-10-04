fc372ffba6598456eacd611a7f3b0c495e3ee320
namuhuchutong <namuhuchutong@gmail.com>
2022-10-04 15:39:48 +0900

    Test: 알림 Fixture 수정

--- src/test/java/com/rollingpaper/ggeujeogggeujeog/common/fixture/NotificationTestFixture.java
+++ src/test/java/com/rollingpaper/ggeujeogggeujeog/common/fixture/NotificationTestFixture.java
@@ -3,6 +3,7 @@ package com.rollingpaper.ggeujeogggeujeog.common.fixture;
 import com.rollingpaper.ggeujeogggeujeog.notification.domain.Notification;
 import com.rollingpaper.ggeujeogggeujeog.notification.domain.NotificationMessage;
 import com.rollingpaper.ggeujeogggeujeog.notification.domain.NotificationStatus;
+import com.rollingpaper.ggeujeogggeujeog.notification.domain.NotificationType;
 
 public class NotificationTestFixture {
 
@@ -10,9 +11,12 @@ public class NotificationTestFixture {
 		public static Notification NOTIFICATION1 = Notification.builder()
 			.id(1L)
 			.message(
-				NotificationMessage.createCommentMessage(
-						"test-noti-content-1",
-						CommentTestFixture.TestComment.COMMENT1.getId())
+				NotificationMessage.builder()
+					.title("TestUser1 - notification title - paper1")
+					.content("TestUser1 - content")
+					.typeId(PaperTestFixture.TestPaper.PAPER1.getBoardId())
+					.type(NotificationType.PAPER)
+					.build()
 			)
 			.status(NotificationStatus.ALIVE)
 			.userId(UserTestFixture.TestUser.USER1.getId())
@@ -21,9 +25,12 @@ public class NotificationTestFixture {
 		public static Notification NOTIFICATION2 = Notification.builder()
 			.id(3L)
 			.message(
-				NotificationMessage.createPaperMessage(
-						"test-noti-paper-1",
-						PaperTestFixture.TestPaper.PAPER1.getId())
+				NotificationMessage.builder()
+					.title("TestUser1 - notification title - comment1")
+					.content("TestUser1 - content")
+					.typeId(CommentTestFixture.TestComment.COMMENT1.getId())
+					.type(NotificationType.COMMENT)
+					.build()
 			)
 			.status(NotificationStatus.DELETED)
 			.userId(UserTestFixture.TestUser.USER1.getId())
