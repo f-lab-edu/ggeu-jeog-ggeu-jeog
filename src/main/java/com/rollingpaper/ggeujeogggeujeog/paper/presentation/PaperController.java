573356a705799062d13b69170c2d3e8b737350ae
namuhuchutong <namuhuchutong@gmail.com>
2022-10-18 15:58:31 +0900

    Refactor: http 메소드 수정

--- src/main/java/com/rollingpaper/ggeujeogggeujeog/paper/presentation/PaperController.java
+++ src/main/java/com/rollingpaper/ggeujeogggeujeog/paper/presentation/PaperController.java
@@ -9,10 +9,8 @@ import javax.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
@@ -65,9 +63,9 @@ public class PaperController {
 		return ResponseEntity.ok(paperResponseDto);
 	}
 
-	@PatchMapping("/boards/{boardId}/rollingpapers/{paperId}")
+	@PostMapping("/boards/{boardId}/rollingpapers/{paperId}")
 	public ResponseEntity<Void> update(
-		@RequestBody @Valid PaperUpdateRequestDto dto,
+		@RequestPart(value = "request") @Valid PaperUpdateRequestDto dto,
 		@RequestPart(value = "image") MultipartFile imageFile,
 		@PathVariable Long paperId
 	) {
