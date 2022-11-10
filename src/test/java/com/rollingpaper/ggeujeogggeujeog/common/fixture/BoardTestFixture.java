ddfe2ec54d3ecd8024534a7f44df710484fca9c5
namuhuchutong <namuhuchutong@gmail.com>
2022-11-10 15:58:34 +0900

    Test: 보드 테스트 픽스처 수정

--- src/test/java/com/rollingpaper/ggeujeogggeujeog/common/fixture/BoardTestFixture.java
+++ src/test/java/com/rollingpaper/ggeujeogggeujeog/common/fixture/BoardTestFixture.java
@@ -1,5 +1,9 @@
 package com.rollingpaper.ggeujeogggeujeog.common.fixture;
 
+import static com.rollingpaper.ggeujeogggeujeog.common.fixture.TagTestFixture.TestTag.*;
+
+import java.util.Arrays;
+
 import com.rollingpaper.ggeujeogggeujeog.board.domain.Board;
 import com.rollingpaper.ggeujeogggeujeog.board.domain.Theme;
 
@@ -12,6 +16,7 @@ public class BoardTestFixture {
 			.theme(Theme.THEME1)
 			.isOpened(false)
 			.userId(UserTestFixture.TestUser.USER1.getId())
+			.tags(Arrays.asList(TAG1, TAG2))
 			.build();
 
 		public static Board BOARD2 = Board.builder()
@@ -20,6 +25,25 @@ public class BoardTestFixture {
 			.theme(Theme.THEME1)
 			.isOpened(false)
 			.userId(UserTestFixture.TestUser.USER2.getId())
+			.tags(Arrays.asList(TAG1))
+			.build();
+
+		public static Board BOARD3 = Board.builder()
+			.id(2L)
+			.boardTitle("Title2")
+			.theme(Theme.THEME1)
+			.isOpened(true)
+			.userId(UserTestFixture.TestUser.USER2.getId())
+			.tags(Arrays.asList(TAG1))
+			.build();
+
+		public static Board BOARD4 = Board.builder()
+			.id(2L)
+			.boardTitle("Title2")
+			.theme(Theme.THEME1)
+			.isOpened(true)
+			.userId(UserTestFixture.TestUser.USER2.getId())
+			.tags(Arrays.asList(TAG2))
 			.build();
 	}
 }
