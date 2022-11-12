ba01e5e87910370fce2760db650422ec8b4d4a3b
namuhuchutong <namuhuchutong@gmail.com>
2022-11-12 16:04:54 +0900

    Feat: 중복 태그, 없는 태그 예외 클래스, 코드, 메시지 추가

---
+++ src/main/java/com/rollingpaper/ggeujeogggeujeog/board/exception/NoSuchTagException.java
@@ -0,0 +1,21 @@
+package com.rollingpaper.ggeujeogggeujeog.board.exception;
+
+import org.springframework.http.HttpStatus;
+
+import com.rollingpaper.ggeujeogggeujeog.common.exception.BaseException;
+import com.rollingpaper.ggeujeogggeujeog.common.exception.ExceptionCode;
+
+public class NoSuchTagException extends BaseException {
+
+	private static final String errorCode = ExceptionCode.NO_SUCH_TAG.getErrorCode();
+	private static final HttpStatus httpStatus = HttpStatus.BAD_REQUEST;
+	private static final String errorMessage = "존재하지 않는 태그입니다.";
+
+	public NoSuchTagException() {
+		this(errorCode, httpStatus, errorMessage);
+	}
+
+	public NoSuchTagException(String errorCode, HttpStatus httpStatus, String errorMessage) {
+		super(errorCode, httpStatus, errorMessage);
+	}
+}
