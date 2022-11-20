47e2fbecaf4244058e9967a73dadef8de211c98a
namuhuchutong <namuhuchutong@gmail.com>
2022-11-21 08:38:22 +0900

    Refactor: domain layer repository 추가

--- src/main/java/com/rollingpaper/ggeujeogggeujeog/authentication/infrastructure/RedisTokenRepository.java
+++ src/main/java/com/rollingpaper/ggeujeogggeujeog/authentication/infrastructure/RedisTokenRepository.java
@@ -6,14 +6,17 @@ import java.util.concurrent.TimeUnit;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Repository;
 
+import com.rollingpaper.ggeujeogggeujeog.authentication.domain.TokenRepository;
+
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 @Repository
-public class TokenRepository {
+public class RedisTokenRepository implements TokenRepository {
 
 	private final RedisTemplate<String, String> redisTemplate;
 
+	@Override
 	public void storeToken(String userEmail, String token) {
 		redisTemplate.opsForValue().set(
 			userEmail,
@@ -22,7 +25,7 @@ public class TokenRepository {
 			TimeUnit.MINUTES
 		);
 	}
-
+	@Override
 	public String getToken(String email) {
 		return Optional.ofNullable(redisTemplate.opsForValue().get(email))
 			.orElseThrow(() -> new IllegalArgumentException("토큰 값이 존재하지 않습니다."));
