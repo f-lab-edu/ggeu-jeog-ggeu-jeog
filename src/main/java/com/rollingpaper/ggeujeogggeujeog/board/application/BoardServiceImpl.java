f4d7206abf96af8d8365cd27199b9aa44fc01ef3
namuhuchutong <namuhuchutong@gmail.com>
2022-11-22 07:36:09 +0900

    Refactor: boardMapper to boardRepository

--- src/main/java/com/rollingpaper/ggeujeogggeujeog/board/application/BoardServiceImpl.java
+++ src/main/java/com/rollingpaper/ggeujeogggeujeog/board/application/BoardServiceImpl.java
@@ -7,9 +7,9 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.rollingpaper.ggeujeogggeujeog.board.domain.Board;
+import com.rollingpaper.ggeujeogggeujeog.board.domain.BoardRepository;
 import com.rollingpaper.ggeujeogggeujeog.board.exception.BoardOwnerException;
 import com.rollingpaper.ggeujeogggeujeog.board.exception.NoSuchBoardException;
-import com.rollingpaper.ggeujeogggeujeog.board.infrastructure.BoardMapper;
 import com.rollingpaper.ggeujeogggeujeog.board.presentation.dto.BoardRequestDto;
 import com.rollingpaper.ggeujeogggeujeog.board.presentation.dto.BoardSearchRequestDto;
 import com.rollingpaper.ggeujeogggeujeog.board.presentation.dto.BoardsResponseDto;
@@ -26,18 +26,18 @@ import lombok.RequiredArgsConstructor;
 public class BoardServiceImpl implements BoardService {
 
     private final UserService userService;
-    private final BoardMapper boardMapper;
+    private final BoardRepository boardRepository;
 
     @Override
     public void register(BoardRequestDto dto, Long userId) {
         User user = userService.getUserById(userId).orElseThrow(NoSuchUserException::new);
-        boardMapper.save(BoardRequestDto.toEntity(dto, user));
+        boardRepository.save(BoardRequestDto.toEntity(dto, user));
     }
 
     @Override
     @Transactional(readOnly = true)
     public List<UserBoardResponseDto> getUserBoard(Long userId) {
-        List<Board> boards = boardMapper.findByUserId(userId);
+        List<Board> boards = boardRepository.findByUserId(userId);
         return boards.stream()
             .map(board -> new UserBoardResponseDto(board.getBoardTitle(),
             board.getTheme(), board.isOpened(), board.getUpdatedDate()))
@@ -48,19 +48,19 @@ public class BoardServiceImpl implements BoardService {
     @Transactional
     public void deleteBoard(Long boardId, User user) {
         checkBoardOwner(boardId, user);
-        boardMapper.delete(boardId);
+        boardRepository.delete(boardId);
     }
 
     @Override
     @Transactional
     public void updateBoard(BoardRequestDto dto, Long boardId, User user) {
         checkBoardOwner(boardId, user);
-        boardMapper.update(BoardRequestDto.toEntity(dto, user));
+        boardRepository.update(BoardRequestDto.toEntity(dto, user));
     }
 
     @Transactional
     public Board checkBoardOwner(Long boardId, User user) throws BaseException {
-        Board board = boardMapper.findById(boardId).orElseThrow(NoSuchBoardException::new);
+        Board board = boardRepository.findById(boardId).orElseThrow(NoSuchBoardException::new);
         if (!board.getUserId().equals(user.getId())) {
             throw new BoardOwnerException();
         }
@@ -70,14 +70,14 @@ public class BoardServiceImpl implements BoardService {
     @Override
     @Transactional(readOnly = true)
     public BoardsResponseDto getBoards(boolean isOpened) {
-        List<Board> boards = boardMapper.findAllBoards(isOpened);
+        List<Board> boards = boardRepository.findAllBoards(isOpened);
         return new BoardsResponseDto(boards);
     }
 
     @Override
     @Transactional(readOnly = true)
     public BoardsResponseDto getBoards(BoardSearchRequestDto dto) {
-        List<Board> boards = boardMapper.findAllTaggedBoards(dto.getTagNames(), dto.isOpened());
+        List<Board> boards = boardRepository.findAllTaggedBoards(dto.getTagNames(), dto.isOpened());
         return new BoardsResponseDto(boards);
     }
 }
