2323d24bfb5498bcf2c72237b79511268c875fbf
namuhuchutong <namuhuchutong@gmail.com>
2023-01-04 15:29:00 +0900

    Chore: 배포 스크립트 변경

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-docker stop rollingpaper
-docker rm rollingpaper
+PROC='ps aux | grep ggeu'
+if [[ $PROC == *"ggeu"* ]]; then
+    echo "Process is running."
+    kill -9 $(ps -ef | grep ggeu | grep -v grep | awk '{print $2}')
+    echo "removed"
+else
+    echo "Process is not running."
+fi
